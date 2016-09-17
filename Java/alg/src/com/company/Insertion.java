de9ea9b5eef367a5d74933893a656a4d9b2b675a
Qin_Shelly <Qin_Shelly@outlook.com>
2016-09-17 14:57:25 +0800

    insertion selection

---
+++ Java/alg/src/com/company/Insertion.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Insertion {
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        for (int i = 1; i < N; i++){
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--){
+                    exch(a, j, j - 1);
+            }
+        }
+
+    }
+
+    private static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j){
+        Comparable t = a[i]; a[i] = a[j]; a[j] = t;
+    }
+
+    private static void show(Comparable[] a){
+        for(int i = 0; i < a.length; i++){
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a){
+        for (int i = 1; i < a.length; i++){
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        In in = new In(args[0]);
+        String[] a = in.readAllStrings();
+
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
