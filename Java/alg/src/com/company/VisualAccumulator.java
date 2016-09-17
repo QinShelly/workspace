bfa54de4297fdb5ded05eca0b2605e06c96cf36c
Qin_Shelly <Qin_Shelly@outlook.com>
2016-09-17 13:55:04 +0800

    visaulaccumulator

---
+++ Java/alg/src/com/company/VisualAccumulator.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class VisualAccumulator {
+    private double total;
+    private int N;
+
+    public VisualAccumulator(int trials, double max){
+        StdDraw.setXscale(0, trials);
+        StdDraw.setYscale(0, max);
+        StdDraw.setPenRadius(0.005);
+    }
+
+    public void addDataValue(double val){
+        N++;
+        total += val;
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.point(N, val);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.point(N, total / N);
+    }
+
+    public static void main(String[] args){
+        int T = Integer.parseInt(args[0]);
+        VisualAccumulator a = new VisualAccumulator(T, 1.0);
+        for (int t = 0; t < T; t++) {
+            a.addDataValue(StdRandom.uniform());
+        }
+        StdOut.println(a);
+    }
+
+    public double mean(){
+        return total/N;
+    }
+
+    public String toString(){
+        return "Mean (" + N + " values): "
+                + String.format("%7.5f", mean());
+    }
+}
