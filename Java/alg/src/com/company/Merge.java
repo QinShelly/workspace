9d68298e3fb3a7b6302bae37141f8064216dd2db
Qin_Shelly <Qin_Shelly@outlook.com>
2016-09-17 16:32:50 +0800

    merge sortcompare

--- Java/alg/src/com/company/Merge.java
+++ Java/alg/src/com/company/Merge.java
@@ -4,14 +4,33 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class Insertion {
-    public static void sort(Comparable[] a){
-        int N = a.length;
-        for (int i = 1; i < N; i++){
-            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--){
-                    exch(a, j, j - 1);
-            }
+public class Merge {
+    private static Comparable[] aux;
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi){
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++){
+            aux[k] = a[k];
         }
+
+        for (int k = lo; k < hi; k++){
+            if      (i > mid)              a[k] = aux[j++];
+            else if (j > hi)               a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
+        }
+    }
+    public static void sort(Comparable[] a){
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo)/2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
     }
 
     private static boolean less(Comparable v, Comparable w){
