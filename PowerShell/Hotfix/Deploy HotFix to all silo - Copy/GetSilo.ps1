7b1cd72d9688cbbe82b10e24ae2f97ea2cd48661
Shelly <ShellyQin@outlook.com>
2016-09-20 03:31:02 -0400

    remove dependecney on sqlps

--- PowerShell/Hotfix/Deploy HotFix to all silo - Copy/GetSilo.ps1
+++ PowerShell/Hotfix/Deploy HotFix to all silo - Copy/GetSilo.ps1
@@ -1,22 +1,29 @@
 $currentPath=Split-Path ((Get-Variable MyInvocation -Scope 0).Value).MyCommand.Path
-import-module "$currentPath\sqlps.ps1"
+#import-module "$currentPath\sqlps.ps1"
 
 function Get-Silo{
-    param($databaseServer="wal1wnfshub.colo.retailsolutions.com",
-    $databaseName="GAHUB",
+    param($databaseServer="ENGV2HHDBQA1.ENG.RSICORP.LOCAL",
+    $databaseName="HUB_FUNCTION_BETA",
     $buildnumer=7056)
 
     $loadInfo = [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
 
     Set-StrictMode -Version Latest
 
-    $sql = @"
-    select DB_SERVERNAME,DB_NAME,siloid,iis_liveserver_url,Cube_Name from RSI_DIM_SILO 
-    where BUILD_NUMBER='$buildnumer' and type='S' and status='A'
+    $cmd = @"
+    select DB_SERVERNAME, DB_NAME, siloid, iis_liveserver_url,Cube_Name from RSI_DIM_SILO 
+    where  type='S' and status='A'
 "@
-    $silos = @(Invoke-Sqlcmd -Query $sql -ServerInstance $databaseServer -database $databaseName -QueryTimeout 65535)
 
-    return $silos
+# "BUILD_NUMBER='$buildnumer' and"
+    $con = "server=$databaseServer;database=$databaseName;Integrated Security=sspi" 
+    $da = new-object System.Data.SqlClient.SqlDataAdapter ($cmd, $con) 
+    $dt = new-object System.Data.DataTable 
+    $da.fill($dt) | out-null 
+
+#    $silos = @(Invoke-Sqlcmd -Query $sql -ServerInstance $databaseServer -database $databaseName -QueryTimeout 65535)
+
+    return $dt
 }
 
- Get-Silo
+$s  = Get-Silo
