7cb6d1c3e6bf48501e8feeb9342d822a73c25511
Qin_Shelly <Qin_Shelly@outlook.com>
2016-10-02 12:00:15 +0800

    r

--- python/webscrapy/ppdai/db_script/vw_ppdai_original.sql
+++ python/webscrapy/ppdai/db_script/vw_ppdai_original.sql
@@ -1,6 +1,6 @@
 
-drop view vw_ppdai;
-create view vw_ppdai as
+drop view vw_ppdai_original;
+create view vw_ppdai_original as
 SELECT p.id,  amount,
 CASE WHEN age <= 39
        AND rate > 18
@@ -10,26 +10,26 @@ CASE WHEN age <= 39
        AND education_level IN ('本科', ' 硕士研究生', '博士研究生', '硕士', '第二学士学位', '博士')
      THEN CASE WHEN education_method IN ('普通', '普通全日制', '研究生')
                THEN CASE
-                   WHEN wsl_rank < 10  THEN 200 + CASE WHEN sex = '女' THEN 40 ELSE 0 END
-                   WHEN wsl_rank < 20  THEN 150 + CASE WHEN sex = '女' THEN 30 ELSE 0 END
-                   WHEN wsl_rank < 40  THEN 100 + CASE WHEN sex = '女' THEN 20 ELSE 0 END
-                   WHEN wsl_rank < 80  THEN 75  + CASE WHEN sex = '女' THEN 15 ELSE 0 END
-                   WHEN wsl_rank < 120 THEN 75  + CASE WHEN sex = '女' THEN 15 ELSE 0 END
-                   WHEN wsl_rank < 150 THEN 50  + CASE WHEN sex = '女' THEN 10 ELSE 0 END
-                   WHEN star >= 6 THEN 50 + CASE WHEN sex = '女' THEN 0 ELSE 0 END
-                   WHEN star >= 5 THEN 50 + CASE WHEN sex = '女' THEN 0 ELSE 0 END
-                   WHEN star >= 4 THEN 50 + CASE WHEN sex = '女' THEN 0 ELSE 0 END
+                   WHEN wsl_rank < 10 THEN 500 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN wsl_rank < 20 THEN 400 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN wsl_rank < 40 THEN 266 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN wsl_rank < 80 THEN 200 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN wsl_rank < 120 THEN 166 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN wsl_rank < 150 THEN 133 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN star >= 6 THEN 333 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN star >= 5 THEN 233 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
+                   WHEN star >= 4 THEN 166 * 2 + CASE WHEN sex = '女' THEN 50 ELSE 0 END
                    WHEN wsl_rank < 200 THEN 0  + CASE WHEN sex = '女' THEN 0 ELSE 0 END
                    ELSE 0
                    END
                  + CASE WHEN (certificates_in_str LIKE '%央行征信报告%' OR certificates_in_str LIKE '%人行征信认证%')
                             AND rate > 18
-                        THEN 0 * 2 ELSE 0 END
-                 + CASE WHEN certificates_in_str LIKE '%个人常用银行流水%' AND rate > 18 THEN 0 * 2 ELSE 0 END
-                 + CASE WHEN certificates_in_str LIKE '%公积金资料%' AND rate > 18 THEN 0 * 2 ELSE 0 END
+                        THEN 51 * 2 ELSE 0 END
+                 + CASE WHEN certificates_in_str LIKE '%个人常用银行流水%' AND rate > 18 THEN 50 * 2 ELSE 0 END
+                 + CASE WHEN certificates_in_str LIKE '%公积金资料%' AND rate > 18 THEN 49 * 2 ELSE 0 END
                WHEN education_method IN ('成人', '自考', '自学考试')
                THEN CASE
-                   	WHEN wsl_rank < 150 THEN 0 + CASE WHEN sex = '女' THEN 0 ELSE 0 END
+                   	WHEN wsl_rank < 150 THEN 51 + CASE WHEN sex = '女' THEN 25 ELSE 0 END
                    	ELSE 0
                    	END
            END
@@ -37,8 +37,8 @@ CASE WHEN age <= 39
 END
 + CASE WHEN ppdai_level = 'C' AND rate = 22 AND age < 40 AND cnt_return_on_time > 10 AND cnt_return_less_than_15 < 3
        AND title NOT LIKE '%闪电%' AND education_level IS NOT NULL
-       THEN 0 * 2 ELSE 0 END
-+ CASE WHEN waiting_to_get_back - 1.2 * amount >= 0 AND rate > 18 THEN 25 * 2 ELSE 0 END
+       THEN 37 * 2 ELSE 0 END
++ CASE WHEN waiting_to_get_back - 1.2 * amount >= 0 AND rate > 18 THEN 66 * 2 ELSE 0 END
        as amount_bid,
        p.sex,
        p.insert_dt,
