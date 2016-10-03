0331339f44190741e04b4e60131f348046ea0ce9
Qin_Shelly <Qin_Shelly@outlook.com>
2016-10-03 15:28:18 +0800

    second

--- python/stock_analyze/analyze_stock.py
+++ python/stock_analyze/analyze_stock.py
@@ -1,14 +1,15 @@
 # coding: utf-8
-
 import tushare as ts
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
+file = "%s.csv" % sys.argv[1]
 # df = ts.get_h_data('601318',start='2015-01-01') #一次性获取全部日k线数据
 # df.to_csv('601318.csv')
 
-df = pd.read_csv('601318.csv')
+df = pd.read_csv(file)
 df = df.sort_values('date')
 df = df.set_index('date')
 df['seenHighest'] = np.NaN
@@ -16,6 +17,8 @@ df['seenLowest'] = np.NaN
 df['secondaryRally'] = np.NaN
 df['naturalRally'] = np.NaN
 df['upwardTrend'] = np.NaN
+df['red'] = np.NaN
+df['black'] = np.NaN
 df['downwardTrend'] = np.NaN
 df['naturalReaction'] = np.NaN
 df['secondaryReaction'] = np.NaN
@@ -25,13 +28,10 @@ seenHighest = 0
 seenLowest = 999
 determineFirstStatus = True
 currentStatus = 'Unknown'
-upwardTrend = 0
-downwardTrend = 0
-naturalReaction = 0
-naturalRally = 0
+upwardTrend = downwardTrend = naturalReaction  = 0
+naturalRally = secondaryRally = secondaryReaction =0
 
 for index, row in df.iterrows():
-
 	if row['high'] > seenHighest:
 		seenHighest = row['high']
 		row['seenHighest'] = row['high']
@@ -39,116 +39,178 @@ for index, row in df.iterrows():
 		seenLowest = row['low']
 		row['seenLowest'] = row['low']
 	if row['high'] > seenLowest + 6 and currentStatus == 'Unknown':
-		row['upwardTrend'] = row['high']
-		upwardTrend = row['high']
+		row['upwardTrend'] = upwardTrend = row['high']
 		currentStatus = 'UpwardTrend'
 	if row['low'] < seenHighest - 6 and currentStatus == 'Unknown':
-		row['downwardTrend'] = row['low']
-		downwardTrend = row['low']
+		row['downwardTrend'] =	downwardTrend = row['low']
 		currentStatus = 'DownwardTrend'
-	
-	# 6.c 6.d (1)
-	# 
+
+# =============================================
+	#  6.g(2)
+	# SecondaryRally -- SecondaryRally
+	if  currentStatus == 'SecondaryRally' and row['high'] > secondaryRally :
+		row['secondaryRally'] = secondaryRally = row['high']
+		row['reason'] = 11
+		continue
+	#  6.g(3)
+	# SecondaryRally -- NaturalRally
+	if  currentStatus == 'SecondaryRally' and row['high'] > naturalRally :
+		currentStatus = 'NatualRally'
+		row['naturalRally'] = naturalRally = row['high']
+		row['reason'] = 12
+		continue
+# =============================================
+	# 6.c(2) 6.d(2)
+	# NatualRally -- NatualRally
 	if  currentStatus == 'NaturalRally' and row['high'] > naturalRally :
 		row['naturalRally'] = naturalRally = row['high']
-		row['reason'] = 1
+		row['reason'] = 22
 		continue
-	# 6.d (2)
-	# UpwardTrend -- continue up 
+	# 6.h(1) NatualRally -- -6 -- secondaryReaction
+	if currentStatus == 'NaturalRally' and row['low'] < naturalRally + 6 \
+	and row['low'] > naturalReaction and naturalReaction > 0:
+		currentStatus = 'SecondaryReaction'
+		row['secondaryReaction'] = secondaryReaction = row['low']
+		row['reason'] = 26
+		continue
+	# 4.d 6.b(1)
+	# NaturalRally -- -6 -- DownwardTrend Or NaturalReaction
+	if currentStatus == 'NaturalRally' and row['low'] < naturalRally - 6:
+		blackNaturalRally = naturalRally
+		row['black'] = 6666
+		if row['low'] < downwardTrend:
+			currentStatus = 'DownwardTrend'
+			row['downwardTrend'] = downwardTrend = row['low']
+			row['reason'] = 24
+		else:
+			currentStatus = 'NaturalReaction'
+			row['naturalReaction'] = naturalReaction = row['low']
+			row['reason'] = 25
+		continue
+	# 5.a
+	# NatualRally -- +3 blackNaturalRally -- UpwardTrend
+	if currentStatus == 'NaturalRally' and 'blackNaturalRally' in locals() \
+	and row['high'] > blackNaturalRally + 3:
+		currentStatus = 'UpwardTrend'
+		row['upwardTrend'] = upwardTrend = row['high']
+		row['reason'] = 23
+		continue
+	#  6.f
+	# NatualRally --  Higher than UpwardTrend -- UpwardTrend
+	if currentStatus == 'NaturalRally' and row['high'] > upwardTrend:
+		currentStatus = 'UpwardTrend'
+		row['upwardTrend'] = upwardTrend = row['high']
+		row['reason'] = 23
+		continue
+# =============================================
+	# 6.d (3)
+	# UpwardTrend -- continue up -- UpwardTrend
 	if currentStatus == 'UpwardTrend' and row['high'] > upwardTrend :
 		row['upwardTrend'] = upwardTrend = row['high']
-		row['reason'] = 2
+		row['reason'] = 33
 		continue
-	# 6.a 6.b (1)
-	if  currentStatus == 'NaturalReaction' and row['low'] < naturalReaction :
-		row['naturalReaction'] = naturalReaction = row['low']
-		row['reason'] = 3
-		continue
-
-	# 6.b (2)
-	# DownwardTrend -- continue down 
-	if  currentStatus == 'DownwardTrend' and row['low'] < downwardTrend :
-		row['downwardTrend'] = downwardTrend = row['low']
-		row['reason'] = 4
-		continue
-
-	# 4.a
+	# 4.a 6.a
 	# UpwardTrend -- -6 -- NatualReaction
 	if currentStatus == 'UpwardTrend' and row['low'] < upwardTrend - 6:
 		redUpwardTrend = upwardTrend
+		row['red'] = 9999
 		row['naturalReaction'] = naturalReaction = row['low']
 		currentStatus = 'NaturalReaction'
-		row['reason'] = 5
+		row['reason'] = 35
 		continue
-	# 4.b
-	# NaturalReaction -- +6 -- UpTrend Or NatualRally
-	if currentStatus == 'NaturalReaction' and row['high'] > naturalReaction + 6:
-		redNaturalReaction = naturalReaction
-		if row['high'] > upwardTrend:
-			currentStatus = 'UpwardTrend'
-			upwardTrend = row['high']
-			row['reason'] = '4.b'
-		else:
-			currentStatus = 'NaturalRally'
-			naturalRally = row['high']
-			row['reason'] = '4.b'
-		row['reason'] = 6
+# =============================================
+	# 6.b (3)
+	# DownwardTrend -- continue down -- DownwardTrend
+	if  currentStatus == 'DownwardTrend' and row['low'] < downwardTrend :
+		row['downwardTrend'] = downwardTrend = row['low']
+		row['reason'] = 44
 		continue
-	# 4.c
+	# 4.c 6.c(1)
 	# DownTrend -- +6 -- NatualRally
 	if currentStatus == 'DownwardTrend' and row['high'] > downwardTrend + 6:
 		blackDownwardTrend = downwardTrend
+		row['black'] = 6666
 		row['naturalRally'] = naturalRally = row['high']
 		currentStatus = 'NaturalRally'
-		row['reason'] = 7
+		row['reason'] = 42
 		continue
-	# 4.d
-	# NaturalRally -- -6 -- DownwardTrend Or NaturalReaction
-	if currentStatus == 'NaturalRally' and row['low'] < naturalRally - 6:
-		blackNatualRally = naturalRally
-		if row['low'] < downwardTrend:
-			currentStatus = 'DownwardTrend'
-			downwardTrend = row['low']
+# =============================================
+	#  6.b(2)
+	# NaturalReaction -- NaturalReaction
+	if  currentStatus == 'NaturalReaction' and row['low'] < naturalReaction :
+		row['naturalReaction'] = naturalReaction = row['low']
+		row['reason'] = 55
+		continue
+	# 6.g(1) NaturalReaction -- +6 -- secondaryRally
+	if currentStatus == 'NaturalReaction' and row['high'] > naturalReaction + 6 \
+	and row['high'] < naturalRally and naturalRally != 999:
+		currentStatus = 'SecondaryRally'
+		row['secondaryRally'] = secondaryRally = row['high']
+		row['reason'] = 51
+		continue
+	# 4.b 6.d(1)
+	# NaturalReaction -- +6 -- UpTrend Or NatualRally
+	if currentStatus == 'NaturalReaction' and row['high'] > naturalReaction + 6:
+		redNaturalReaction = naturalReaction
+		row['red'] = 9999
+		if row['high'] > upwardTrend:
+			currentStatus = 'UpwardTrend'
+			row['upwardTrend'] = upwardTrend = row['high']
+			row['reason'] = 53
 		else:
-			currentStatus = 'NaturalReaction'
-			naturalReaction = row['low']
-		row['reason'] = 8
-		continue
-	# 5.a
-	if currentStatus == 'NaturalRally' and 'blackNatualRally' in locals() and row['high'] > blackNatualRally + 3:
-		currentStatus = 'UpwardTrend'
-		upwardTrend = row['high']
-		row['reason'] = 9
+			currentStatus = 'NaturalRally'
+			row['naturalRally'] = naturalRally = row['high']
+			row['reason'] = 52
 		continue
 	# 5.b
-	if currentStatus == 'NaturalReaction' and 'redNaturalReaction' in locals() and row['low'] < redNaturalReaction - 3:
+	# NaturalReaction -- -3 redNaturalReaction-- DownwardTrend
+	if currentStatus == 'NaturalReaction' and 'redNaturalReaction' in locals() \
+	and row['low'] < redNaturalReaction - 3:
 		currentStatus = 'DownwardTrend'
-		downwardTrend = row['low']
-		row['reason'] = 10
+		row['downwardTrend'] = downwardTrend = row['low']
+		row['reason'] = 54
 		continue
 	#  6.e
 	if currentStatus == 'NaturalReaction' and row['low'] < downwardTrend:
 		currentStatus = 'DownwardTrend'
 		row['downwardTrend'] = downwardTrend = row['low']
-		row['reason'] = 11
+		row['reason'] = 54
 		continue
-	#  6.f
-	if currentStatus == 'NaturalRally' and row['high'] > upwardTrend:
-		currentStatus = 'UpwardTrend'
-		row['upwardTrend'] = upwardTrend = row['high']
-		row['reason'] = 12
+# =============================================
+	#  6.h(2)
+	# SecondaryReaction -- SecondaryReaction
+	if  currentStatus == 'SecondaryReaction' and row['low'] < secondaryReaction :
+		row['secondaryReaction'] = secondaryReaction = row['low']
+		row['reason'] = 66
 		continue
-	# 6.g
-	if currentStatus == 'NaturalReaction':
+	#  6.h(3)
+	# SecondaryReaction -- NaturalReaction
+	if  currentStatus == 'SecondaryReaction' and row['low'] < naturalReaction :
 		currentStatus = 'NaturalReaction'
+		row['naturalReaction'] = naturalReaction = row['low']
+		row['reason'] = 65
+		continue
+#=============================================
 
 for index, row in df.iterrows():
-	if pd.notnull(row['naturalRally']) or pd.notnull(row['upwardTrend']) or \
-		pd.notnull(row['naturalReaction']) or pd.notnull(row['downwardTrend']):
-		print "%s %6.2f %6.2f %10s| %6.2f %6.2f %6.2f %6.2f" % (index, row['high'], row['low'], row['reason'],\
-		 row['naturalRally'],row['upwardTrend'], row['downwardTrend'],row['naturalReaction'] )
+	if pd.notnull(row['naturalRally']) or pd.notnull(row['upwardTrend'])  \
+		or pd.notnull(row['naturalReaction']) or pd.notnull(row['downwardTrend']) \
+		or pd.notnull(row['secondaryRally']) or pd.notnull(row['secondaryReaction']):
+		print "%s %6.2f %6.2f %10s| %6.2f %6.2f %6.2f || %6.2f %6.2f %6.2f | %7.0f %7.0f" \
+		 % (index, row['high'], row['low'], row['reason'],\
+		 row['secondaryRally'], row['naturalRally'],row['upwardTrend'], \
+		 row['downwardTrend'],row['naturalReaction'],row['secondaryReaction'] ,row['black'],row['red'])
 
-#print df[any((np.isfinite(df['upwardTrend'])) or any((np.isfinite(df['downwardTrend'])))]
+# for index, row in df.iterrows():
+# 	print "%s %6.2f %6.2f %10s| %6.2f %6.2f %6.2f || %6.2f %6.2f %6.2f | %7.0f %7.0f" \
+# 	 % (index, row['high'], row['low'], row['reason'],\
+# 	 row['secondaryRally'], row['naturalRally'],row['upwardTrend'], \
+# 	 row['downwardTrend'],row['naturalReaction'],row['secondaryReaction'] ,row['black'],row['red'])
+
+
+# for index, row in df.iterrows():
+# 	print "%s %6.2f %6.2f %10s| %6.2f %6.2f %6.2f %6.2f" % (index, row['high'], row['low'], row['reason'],\
+# 		 row['naturalRally'],row['upwardTrend'], row['downwardTrend'],row['naturalReaction'] )
 
 #df.plot(x='date', y='close')
 #plt.show()
