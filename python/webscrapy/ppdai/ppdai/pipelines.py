fcf73139b1a6588f41d6b9a87154aedd6d212789
Qin_Shelly <Qin_Shelly@outlook.com>
2017-02-19 17:11:34 +0800

    mysql code

--- python/webscrapy/ppdai/ppdai/pipelines.py
+++ python/webscrapy/ppdai/ppdai/pipelines.py
@@ -6,6 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import sqlite3
+import MySQLdb
 
 class PpdaiPipeline(object):
     def process_item(self, item, spider):
@@ -80,5 +81,57 @@ class PpdaiPipeline(object):
 
         # We can also close the connection if we are done with it.
         # Just be sure any changes have been committed or they will be lost.
+        conn.close()
+
+        conn=MySQLdb.connect(host='localhost',user='root',passwd='',db='ppdai',port=3306)
+        cur=conn.cursor()
+
+        value=[item['id'], item['rank'], item['title'][0], item['brate'][0], item['qty']
+                   , item['limitTime'][0]
+                   , "purpose"
+                   , item['sex']
+                   , item['age']
+                   , "marriage"
+                   , item['education']
+                   , item['house']
+                   , item['car']
+                   , item['school']
+                   , item['study_level']
+                   , item['study_format']
+                   , item['detail'], item['hukou'], item['audit'], item['pay_clear'], item['over1_15'],item['over15plus']
+                   , item['total_borrow'],item['to_pay'],item['to_receive']]
+        cur.execute("INSERT INTO ppdai (id , ppdai_level , title , rate , amount "
+                  ", limitTime "
+                  ", purpose "
+                  ", sex "
+                  ", age "
+                  ", marriage "
+                  ", education "
+                  ", house "
+                  ", car "
+                  ", school "
+                  ", education_level "
+                  ", education_method "
+                  ", detail, hukou , certificates_in_str, cnt_return_on_time, cnt_return_less_than_15, over15plus "
+                  ", total_borrow, waiting_to_pay, waiting_to_get_back) "
+                  "VALUES (%s,%s,%s,%s,%s"
+                  ",%s,%s,%s,%s,%s,%s"
+                  ",%s,%s,%s,%s,%s"
+                  ",%s, %s ,%s, %s, %s, %s"
+                  ",%s ,%s ,%s)",value) 
+        conn.commit()
+
+        sql = "select count(*) from vw_ppdai where id = '%s' and amount_bid > 0" % item['id']
+        cur.execute(sql)
+        result=cur.fetchone()
+        if result[0] == 1:
+          print "it is a bid :)"
+          sql = "INSERT INTO bidProcess(id,insert_dt) values ('%s',now())" % item['id']
+          cur.execute(sql)
+          conn.commit()
+        else:
+          print "not a bid :("
+
+        cur.close()
         conn.close()
         return item
