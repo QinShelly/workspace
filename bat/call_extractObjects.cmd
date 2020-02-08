4734ec9e561c64bcbdb11e321163048c1c140f58
shelly_qin <shelly_qin@outlook.com>
2020-02-08 15:34:34 +0800

    a

--- bat/call_extractObjects.cmd
+++
@@ -1,2 +0,0 @@
-for %%i in (dbAudit, dbCSMART, dbCSMARTSTaging, dbCWPMetadata, dbFactory, dbFactoryInterface, dbFactoryStaging, dbSincera
-, dbSinceraLog, dbSLAReports, dbtest, dbWarehouse, dbWarehouseInterface, dbWarehouseStaging) do a.bat dailyplanettest %%i
